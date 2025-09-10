ba9790970135b8ca44eb954bc503f53ede87b7ef
David Weiss <dweiss2@gmail.com>
2025-09-09 20:58:16 -0500

    Permutations trait

--- src/combinatorics.rs
+++ src/combinatorics.rs
@@ -1,28 +1,72 @@
 use std::{collections::HashSet, hash::Hash};
 
-/// Generate all permutations of array of items where items can repeat in the array.
-pub fn permutations_with_duplicates<N: Eq + Hash + Clone>(items: &[N]) -> HashSet<Vec<N>> {
-    let mut items = items.to_vec();
-    let mut perms = HashSet::new();
-    perms.insert(items.clone());
-    let mut state = vec![0; items.len()];
-    let mut idx = 1;
-    while idx < items.len() {
-        if state[idx] < idx {
-            if idx % 2 == 0 && items[0] != items[idx] {
-                items.swap(0, idx);
-            } else if idx % 2 == 1 && items[state[idx]] != items[idx] {
-                items.swap(state[idx], idx);
-            }
-            perms.insert(items.clone());
-            state[idx] += 1;
-            idx = 1;
-        } else {
-            state[idx] = 0;
-            idx += 1;
+pub trait Permutations<T> {
+    fn permutations(&self) -> PermutationsIterator<T>;
+}
+
+impl<T: Clone + Hash + Eq> Permutations<T> for Vec<T> {
+    fn permutations(&self) -> PermutationsIterator<T> {
+        PermutationsIterator::new(self)
+    }
+}
+
+impl<T: Clone + Hash + Eq> Permutations<T> for &[T] {
+    fn permutations(&self) -> PermutationsIterator<T> {
+        PermutationsIterator::new(self)
+    }
+}
+
+pub struct PermutationsIterator<T> {
+    items: Vec<T>,
+    perms: HashSet<Vec<T>>,
+    state: Vec<usize>,
+    first: bool,
+}
+
+impl<T: Clone + Hash + Eq> PermutationsIterator<T> {
+    fn new(items: &[T]) -> Self {
+        let mut perms = HashSet::new();
+        perms.insert(items.into());
+        let state = vec![0; items.len()];
+        Self {
+            items: items.into(),
+            first: true,
+            perms,
+            state,
         }
     }
-    perms
+}
+
+impl<T: Clone + Hash + Eq> Iterator for PermutationsIterator<T> {
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.first {
+            self.first = false;
+            return Some(self.items.clone());
+        }
+
+        let mut idx = 1;
+
+        while idx < self.items.len() {
+            if self.state[idx] < idx {
+                if idx % 2 == 0 && self.items[0] != self.items[idx] {
+                    self.items.swap(0, idx);
+                } else if idx % 2 == 1 && self.items[self.state[idx]] != self.items[idx] {
+                    self.items.swap(self.state[idx], idx);
+                }
+                self.state[idx] += 1;
+                if self.perms.insert(self.items.clone()) {
+                    return Some(self.items.clone());
+                };
+                idx = 1;
+            } else {
+                self.state[idx] = 0;
+                idx += 1;
+            }
+        }
+        None
+    }
 }
 
 #[cfg(test)]
@@ -30,15 +74,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_permutations_with_duplicates() {
-        assert_eq!(10, permutations_with_duplicates(&[1, 1, 2, 2, 2]).len());
+    fn test_permutations_trait() {
+        assert_eq!(10, vec![1, 2, 1, 2, 1].permutations().count());
     }
 
     #[test]
-    fn test_permutations_with_duplicates_long() {
-        assert_eq!(
-            9240,
-            permutations_with_duplicates(&[1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3]).len()
-        );
+    fn test_permutations_with_duplicates() {
+        assert_eq!(120, [1, 2, 3, 4, 5].as_slice().permutations().count());
     }
 }
