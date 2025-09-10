40d166580beef780a26812d5d714f17ec2e320ac
David Weiss <dweiss2@gmail.com>
2025-09-09 21:01:24 -0500

    Fixed bad merge

--- src/combinatorics.rs
+++ src/combinatorics.rs
@@ -33,26 +33,6 @@ impl<T: Clone + Hash + Eq> PermutationsIterator<T> {
             first: true,
             perms,
             state,
-/// Generate all unique permutations of array of items where items can repeat in the array.
-pub fn permutations_with_duplicates<N: Eq + Hash + Clone>(items: &[N]) -> HashSet<Vec<N>> {
-    let mut items = items.to_vec();
-    let mut perms = HashSet::new();
-    perms.insert(items.clone());
-    let mut state = vec![0; items.len()];
-    let mut idx = 1;
-    while idx < items.len() {
-        if state[idx] < idx {
-            if idx % 2 == 0 && items[0] != items[idx] {
-                items.swap(0, idx);
-            } else if idx % 2 == 1 && items[state[idx]] != items[idx] {
-                items.swap(state[idx], idx);
-            }
-            perms.insert(items.clone());
-            state[idx] += 1;
-            idx = 1;
-        } else {
-            state[idx] = 0;
-            idx += 1;
         }
     }
 }
